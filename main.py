484244f6b3ceab6b97c20a7311df9a4d96743ce1
kamil-korwiel <kamilkorwiel@gmail.com>
2025-03-24 11:07:50 +0100

    feat: generate html using marddown

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+import markdown
+from pathlib import Path 
+
 
 
 app = FastAPI()
@@ -12,4 +15,19 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
-    return templates.TemplateResponse(request=request, name="index.html")
+    html = ""
+    markdown_path = Path("./data/about.md")
+
+    try:
+        with markdown_path.open('r', encoding='utf-8') as file:
+            text_md = file.read()
+            html = markdown.markdown(text_md, extensions=['fenced_code', 'tables'])
+    except FileNotFoundError:
+        html = "<p>Error: Markdown file not found.</p>"
+    
+    return templates.TemplateResponse(request=request, name="site/index.html", context={"html" : html})
+
+
+@app.get("/music-i-listen", response_class=HTMLResponse)
+async def my_music(request: Request):
+    return templates.TemplateResponse(request=request, name="errors/work_in_progress.html",)
