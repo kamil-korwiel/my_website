0fb1fcb2313c319de9f08838930f6e82e7a041c8
kamil-korwiel <kamilkorwiel@gmail.com>
2025-04-01 11:11:09 +0200

    feat: add a contact page

--- main.py
+++ main.py
@@ -16,106 +16,65 @@ def get_markdown(path:Path) -> str:
     return text
     
 
-
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
 
-markdown_path_about_short = Path("./data/about/about_short.md")
+markdown_path_about = Path("./data/index/about/about.md")
 
-markdown_path_python_short = Path("./data/knowledge/python_short.md")
-markdown_path_testing_short = Path("./data/knowledge/testing_short.md")
-markdown_path_others_short = Path("./data/knowledge/others_short.md")
+markdown_path_python = Path("./data/index/knowledge/python.md")
+markdown_path_testing = Path("./data/index/knowledge/testing.md")
+markdown_path_others = Path("./data/index/knowledge/others.md")
 
-markdown_path_genkaraoke_short = Path("./data/projects/genkaraoke_short.md")
-markdown_path_test_short = Path("./data/projects/testing_short.md")
-markdown_path_scraping_short = Path("./data/projects/scraping_short.md")
-
-
-markdown_path_about_long = Path("./data/about/about_long.md")
-
-markdown_path_python_long = Path("./data/knowledge/python_long.md")
-markdown_path_testing_long = Path("./data/knowledge/testing_long.md")
-markdown_path_others_long = Path("./data/knowledge/others_long.md")
-
-markdown_path_genkaraoke_long = Path("./data/projects/genkaraoke_long.md")
-markdown_path_test_long = Path("./data/projects/testing_long.md")
-markdown_path_scraping_long = Path("./data/projects/scraping_long.md")
-
+markdown_path_genkaraoke = Path("./data/index/projects/genkaraoke.md")
+markdown_path_test = Path("./data/index/projects/testing.md")
+markdown_path_scraping = Path("./data/index/projects/scraping.md")
 
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
-
-    knows_long = [
-        Know(
-            name="Python",
-            path_img="350px/Python-Blue-350.png",
-            text=get_markdown(markdown_path_python_long)
-        ),
-        Know(
-            name="Testing",
-            path_img="350px/Test-Green-350.png",
-            text=get_markdown(markdown_path_testing_long)
-        ),
-        Know(
-            name="Other",
-            path_img="350px/Linux-orange-350.png",
-            text=get_markdown(markdown_path_others_long)
-        ),
-    ]
     knows_short = [
         Know(
             name="Python",
             path_img="350px/Python-Blue-350.png",
-            text=get_markdown(markdown_path_python_short)
+            text=get_markdown(markdown_path_python)
         ),
         Know(
             name="Testing",
             path_img="350px/Test-Green-350.png",
-            text=get_markdown(markdown_path_testing_short)
+            text=get_markdown(markdown_path_testing)
         ),
         Know(
             name="Other",
             path_img="350px/Linux-orange-350.png",
-            text=get_markdown(markdown_path_others_short)
+            text=get_markdown(markdown_path_others)
         ),
-    ]
-    
-    projects_long = [
-        get_markdown(markdown_path_genkaraoke_long),
-        get_markdown(markdown_path_test_long),
-        get_markdown(markdown_path_scraping_long),
     ]
 
     projects_short = [
-        get_markdown(markdown_path_genkaraoke_short),
-        get_markdown(markdown_path_test_short),
-        get_markdown(markdown_path_scraping_short),
+        get_markdown(markdown_path_genkaraoke),
+        get_markdown(markdown_path_test),
+        get_markdown(markdown_path_scraping),
     ]
 
     context = {
-        "short":{
-            "about": get_markdown(markdown_path_about_short),
-            "knows": [know.__dict__ for know in knows_short],
-            "projects": "\n".join(projects_short)
-            
-        },
-        "long": {
-            "about": get_markdown(markdown_path_about_long),
-            "knows": [know.__dict__ for know in knows_long],
-            "projects": "\n".join(projects_long)
-        }
+        "about": get_markdown(markdown_path_about),
+        "knows": [know.__dict__ for know in knows_short],
+        "projects": "\n".join(projects_short)
     }
     return templates.TemplateResponse(request=request, name="site/index.html", context=context)
 
 
-@app.get("/cool-music", response_class=HTMLResponse)
+@app.get("/projects", response_class=HTMLResponse)
 async def my_music(request: Request):
-    return templates.TemplateResponse(request=request, name="errors/work_in_progress.html")
+    return templates.TemplateResponse(request=request, name="/card.html")
 
-@app.get("/notes", response_class=HTMLResponse)
-async def my_music(request: Request):
-    return templates.TemplateResponse(request=request, name="errors/work_in_progress.html")
+@app.get("/contact", response_class=HTMLResponse)
+async def contact(request: Request):
+    return templates.TemplateResponse(request=request, name="site/contact.html")
+
+@app.get("/blog", response_class=HTMLResponse)
+async def blog(request: Request):
+    return templates.TemplateResponse(request=request, name="site/blog.html")
